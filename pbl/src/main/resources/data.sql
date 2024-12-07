14d362d326fdff5eb5ca86732ab9cb4e4a68800a
Sneha Veerbhan <146728815+SnehaVeerbhan@users.noreply.github.com>
2024-12-07 11:12:41 +0530

    Update data.sql

--- pbl/src/main/resources/data.sql
+++ pbl/src/main/resources/data.sql
@@ -1,2 +1,1 @@
-INSERT INTO task (title, description, completed) VALUES ('Learn Spring Boot', 'Complete the Spring Boot tutorial', false);
-INSERT INTO task (title, description, completed) VALUES ('Build a Project', 'Create a Spring Boot project', false);
+
